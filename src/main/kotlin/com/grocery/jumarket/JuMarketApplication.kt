4cdce20f58b0ab2e5e5c4679fd3b5f4e9173e4c9
jxxErick <ericksdantas144@gmail.com>
2023-07-07 21:59:27 -0300

    first

---
+++ src/main/kotlin/com/grocery/jumarket/JuMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.grocery.jumarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JuMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<JuMarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/grocery/jumarket/JuMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grocery.jumarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JuMarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
