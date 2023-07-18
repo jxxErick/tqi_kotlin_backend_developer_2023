132a173730ef246003ba223dd87e4b8595ff5ec7
jxxErick <ericksdantas144@gmail.com>
2023-07-17 21:46:19 -0300

    fix

--- src/main/kotlin/com/grocery/jumarket/service/exception/BusinessException.kt
+++ src/main/kotlin/com/grocery/jumarket/service/exception/BusinessException.kt
@@ -1,3 +1,7 @@
 package com.grocery.jumarket.service.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.CONFLICT)
 class BusinessException(override val message: String?) : RuntimeException()
