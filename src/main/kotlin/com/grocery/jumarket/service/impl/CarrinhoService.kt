1114d69596df05033c974c15bb6c29fd2b0a4ed7
jxxErick <ericksdantas144@gmail.com>
2023-07-23 16:09:42 -0300

    carrinhoService tirando comentários q eu fiz

--- src/main/kotlin/com/grocery/jumarket/service/impl/CarrinhoService.kt
+++ src/main/kotlin/com/grocery/jumarket/service/impl/CarrinhoService.kt
@@ -24,23 +24,18 @@ class CarrinhoService (
 ): ICarrinhoService {
 
     override fun adicionarItemAoCarrinho(usuarioId: Long, produtoId: Long, quantidade: Long) {
-        // Verifica se o usuário existe
         val usuario = usuarioRepository.findById(usuarioId)
             .orElseThrow { NotFoundException("Usuário não encontrado") }
 
-        // Verifica se o produto existe
         val produto = produtoRepository.findById(produtoId)
             .orElseThrow { NotFoundException("Produto não encontrado") }
 
-        // Verifica se o produto possui estoque suficiente
         if (produto.quantidadeEstoque < quantidade) {
             throw EstoqueInsuficienteException("Não há estoque suficiente para adicionar o produto ao carrinho.")
         }
 
-        // Buscar o carrinho do usuário, caso ele não tenha, cria um
         val carrinho = usuario.carrinho ?: criarCarrinhoCasoAdicioneProduto(usuario)
 
-        // Adiciona o produto ao carrinho
         carrinho.adicionarItem(ItemCarrinho(produto, quantidade, produto.precoUnitario))
         carrinhoRepository.save(carrinho)
     }
@@ -55,22 +50,19 @@ class CarrinhoService (
             throw NotFoundException("Produto não encontrado no carrinho.")
         }
 
-        // Remove o item do carrinho
         carrinho.removerItem(produtoId)
         carrinhoRepository.save(carrinho)
 
-        // Caso o carrinho fique vazio, apaga ele
         if (carrinho.itens.isEmpty()) {
             carrinhoRepository.delete(carrinho)
         }
     }
 
     override fun listarItensPorUsuario(usuarioId: Long): List<ItemCarrinho> {
-        // Verifica se o usuário existe
+
         val usuario = usuarioRepository.findById(usuarioId)
             .orElseThrow { NotFoundException("Usuário não encontrado") }
 
-        // Verifica se o usuário possui carrinho
         val carrinho = usuario.carrinho
             ?: throw NotFoundException("Carrinho não encontrado para o usuário")
 
